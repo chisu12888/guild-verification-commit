2b964dfb4eb32b7ef3c083c76b8e7c40457b253e
chisu12888 <130205145+chisu12888@users.noreply.github.com>
2026-03-07 04:37:46 +0800

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:33:04.725Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T20:37:45.249Z
+Commit: Fix CSS styling bug on mobile devices
