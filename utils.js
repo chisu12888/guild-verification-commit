f342b4293c7b284f8ae82aab9abacd22f28f3f9e
chisu12888 <130205145+chisu12888@users.noreply.github.com>
2026-03-07 04:31:41 +0800

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:31:32.266Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-06T20:31:40.298Z
+Commit: Resolve null pointer exception in user service
